39d92c23d841d28833d0e106b885d14c33a0ec7d
Willie Suggs <klbooker09@gmail.com>
2017-05-27 21:33:49 -0500

    Add files via upload

---
+++ FindingAValueInAnArraylist.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package findingavalueinanarraylist;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+/**
+ *
+ * @author kendrabooker
+ */
+public class FindingAValueInAnArraylist {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+        ArrayList<Integer> arrList = new ArrayList<>();
+        Random r = new Random();
+        
+        do{
+        
+            Integer num = 1 + r.nextInt(59);
+            arrList.add(num);
+            
+        }while(arrList.size() < 10);
+        
+        System.out.println("arraylist: " + arrList);
+        
+        System.out.print("value to find: ");
+        int input = keyboard.nextInt();
+        
+        
+        if(arrList.contains(input)){
+        
+            System.out.println(input + " is in the arrayList");
+        }else{
+        
+            System.out.println("that value is not in the arrayList");
+        }
+    
+    }
+    
+}
